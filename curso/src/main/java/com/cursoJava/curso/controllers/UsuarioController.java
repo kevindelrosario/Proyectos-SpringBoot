910c6dc9f75367f0f39fe5d3f7a5a215e6b9d7d0
kevinfmdelrosario <kf_montero@rotorbike.com>
2023-11-23 08:52:26 +0100

    Subida del curso

---
+++ curso/src/main/java/com/cursoJava/curso/controllers/UsuarioController.java
@@ -0,0 +1,14 @@
+package com.cursoJava.curso.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+
+    @RequestMapping(value ="prueba")
+    public String prueba() {
+        return  "hola";
+    }
+}
